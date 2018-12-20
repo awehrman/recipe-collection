656dc6ced7e9e1ce8500590ce421d9c7cc1641f0
Alexandra Wehrman <allie.wehrman@gmail.com>
2018-12-20 10:24:51 -0600

    fixed untracked files

--- server/bak/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-	res.send({ response: "Connected" }).status(200);
-});
-
-module.exports = router;
