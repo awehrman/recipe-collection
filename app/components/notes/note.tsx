5074f5a155f386677b7c4439af8f24f14b97f9e3
Allie Wehrman <allie.wehrman@gmail.com>
2022-05-21 14:25:19 -0500

    view more/less

--- app/components/notes/note.tsx
+++ app/components/notes/note.tsx
@@ -1,43 +1,74 @@
-import pretty from 'pretty';
-import React from 'react';
+import React, { useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import pretty from 'pretty';
 import styled from 'styled-components';
 
+import { Button } from '../common';
 import { NotePageProps } from './types';
 
+type ContentProps = {
+  isExpanded: boolean;
+};
+
 const Note: React.FC<NotePageProps> = ({ note }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function handleNoteViewClick() {
+    setIsExpanded(!isExpanded);
+  }
+
   return (
     <Wrapper>
       {/* Title */}
       <Title>{note.title}</Title>
 
-       {/* Content */}
-       <ContentStyles>
+      {/* Content */}
+      <ContentStyles isExpanded={isExpanded}>
         <SyntaxHighlighter
           className='highlighter'
-          language='html'
+          language='vbscript-html'
+          style={atomOneDark}
+          wrapLongLines
         >
-          {pretty(`${note.content}`)}
+          {pretty(note.content ?? ``)}
         </SyntaxHighlighter>
       </ContentStyles>
+
+      {/* View More/Less */}
+      <StyledButton
+        type='button'
+        onClick={handleNoteViewClick}
+        label={isExpanded ? 'View Less' : 'View More'}
+      />
     </Wrapper>
   );
 };
 
 export default Note;
 
+const StyledButton = styled(Button)`
+  &&& {
+    margin: 0;
+    margin-top: 10px;
+  }
+`;
+
 const Wrapper = styled.div`
-	margin: 20px 0;
-	max-width: 850px;
+  margin: 20px 0;
+  max-width: 850px;
 
   background: ${({ theme }) => theme.colors.headerBackground};
-	padding: 24px;
-	margin: 20px 0;
-	max-width: 850px;
-	border-radius: 5px;
-	display: flex;
-	justify-content: space-between;
-	flex-wrap: wrap;
+  padding: 24px;
+  margin: 20px 0;
+  max-width: 850px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  position: relative;
+  overflow: hidden;
 `;
 
 const Title = styled.h2`
@@ -47,12 +78,13 @@ const Title = styled.h2`
   margin: 0;
 `;
 
-const ContentStyles = styled.div`
-	margin-top: 10px;
-	font-size: 10px;
+const ContentStyles = styled.div<ContentProps>`
+  font-size: 10px;
   width: 100%;
+  overflow: hidden;
+  height: ${({ isExpanded }) => isExpanded ? 'unset' : '200px' };
 
-	pre {
-		white-space: pre-wrap;
-	}
+  pre {
+    white-space: pre-wrap;
+  }
 `;
